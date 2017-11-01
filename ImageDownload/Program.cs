290b8855c572c0e5115cc8fae79a516017621ba3
Salvador Sastre <chavosastre@gmai.com>
2017-11-01 17:57:51 -0600

    Update

--- ImageDownload/Program.cs
+++ ImageDownload/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ImageDownload
@@ -10,6 +13,32 @@ namespace ImageDownload
     {
         static void Main(string[] args)
         {
+            try
+            {
+
+                WebClient webClient = new WebClient();
+                // Proxy
+                //webClient.Proxy = new WebProxy("190.109.64.4:3128");
+                // My Document Folder with a new folder
+                String newFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/ExampleFolder/";
+                Console.WriteLine("New Folder:" + newFolder);
+                // Create the Folder
+                if (!Directory.Exists(newFolder))
+                {
+                    Directory.CreateDirectory(newFolder);
+                    Console.WriteLine("Folder created: " + newFolder);
+                }
+                // Download the File
+                webClient.DownloadFile("https://www.google.com.mx/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png", newFolder + "googlelogo_color_272x92dp.png");
+                Console.WriteLine("File downloaded");
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine("Problem: " + ex.Message);
+
+            }
+            Console.ReadKey();
         }
+
     }
 }
